f4326c27f2b1a1c5af08467740ec0476a22f9e75
imcloudwu <imcloudwu@gmail.com>
2013-11-05 15:20:50 +0800

    aspose

---
+++ AsposeUse/AsposeUse/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace AsposeUse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(247, 238);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.check);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "名稱";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "數量";
+            this.Column2.Name = "Column2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "產生變數";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(95, 296);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 12);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.gotoLink);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "清除檔案";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 336);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Excel功能變數產生器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AsposeUse/AsposeUse/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Aspose.Cells;
+
+namespace AsposeUse
+{
+    public partial class Form1 : Form
+    {
+        String path = @"D:\\testExcel.xls";
+        Dictionary<string, string> mapDict = new Dictionary<string, string>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+                row = new DataGridViewRow();
+                row.CreateCells(dataGridView1);
+                row.Cells[0].Value = "第" + i + "項名稱";
+                row.Cells[1].Value = "第" + i + "項內容";
+                dataGridView1.Rows.Add(row);
+            }
+            */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            commitExcel();
+        }
+
+        private void setValue()
+        {
+            mapDict.Clear();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                //???
+                if (row.IsNewRow) continue;
+                if (row.Cells[0].Value != null && row.Cells[1].Value != null)
+                {
+                    String key = row.Cells[0].Value.ToString();
+                    String value = row.Cells[1].Value.ToString();
+                    mapDict.Add(key, value);
+                }
+            }
+        }
+
+        private void commitExcel()
+        {
+            setValue();
+            Aspose.Cells.Workbook workbook = new Aspose.Cells.Workbook();
+            Worksheet worksheet = workbook.Worksheets[0];
+            Cells cells = worksheet.Cells;
+            int count = 0;
+
+            foreach (KeyValuePair<String, String> s in mapDict)
+            {
+
+                for (int i = 0; i <= Convert.ToInt32(s.Value); i++)
+                {
+                    if (i == 0)
+                    {
+                        cells[i, count].PutValue(s.Key);
+                    }
+                    else
+                    {
+                        cells[i, count].PutValue("[[" + s.Key + i + "]]");
+                    }
+                }
+                count++;
+            }
+
+            
+            try
+            {
+                workbook.Save(path);
+                label1.Text = "輸出到:";
+                linkLabel1.Text = path;
+                System.Diagnostics.Process.Start(path);
+            }
+            catch
+            {
+                MessageBox.Show("請關閉現有的" + path);
+            }
+            
+            
+        }
+
+        private void commitWord()
+        {
+            setValue();
+            Aspose.Words.Document doc = new Aspose.Words.Document(new MemoryStream(Properties.Resources.space));
+            Aspose.Words.DocumentBuilder bu = new Aspose.Words.DocumentBuilder(doc);
+
+            bu.StartTable();
+
+            foreach (KeyValuePair<String, String> k in mapDict)
+            {
+                bu.InsertCell();
+                bu.Write(k.Key);
+                bu.InsertCell();
+                bu.Write(k.Value);
+                bu.EndRow();
+            }
+
+
+
+            foreach (KeyValuePair<String, String> k in mapDict)
+            {
+                string str = string.Format("MERGEFIELD {0}" + @"\* MERGEFORMAT ", k.Value + "Item " + "value");
+                string str1 = string.Format("«{0}»", k.Key + "Item " + "key");
+                bu.InsertCell();
+                bu.InsertField(str, str1);
+            }
+
+
+
+            bu.EndRow();
+
+            String path = "D:\\akbbb8.doc";
+            doc.Save(path);
+            System.Diagnostics.Process.Start(path);
+
+
+        }
+
+        private void check(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+
+            if (dataGridView1.Rows[e.RowIndex].IsNewRow) return; //判斷編輯行[如果是新記錄則退出]
+
+            decimal dci;
+
+            if (e.ColumnIndex == 1)   //指定datagridview 需限制輸入的列
+            {
+
+                if (e.FormattedValue != null && e.FormattedValue.ToString().Length > 0)   //确定活動的單元格是否有值輸入
+                {
+
+                    if (!decimal.TryParse(e.FormattedValue.ToString(), out dci) || dci < 0)  //判斷輸入的值是否為數值
+                    {
+
+                        e.Cancel = true;   //取消驗證事件作業,返回輸入狀態.
+
+                        MessageBox.Show("請輸入數字", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void gotoLink(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(this.linkLabel1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(path))
+            {
+                try
+                {
+                    File.Delete(path);
+                    label1.Text = "已刪除" + path;
+                    linkLabel1.Text = "";
+                }
+                catch
+                {
+                    MessageBox.Show("請關閉現有的" + path);
+                }
+
+                
+            }
+        }
+
+
+    }
+}
